90230903e5f1e55c0291ea31b233ec8fb8d7ad19
wangqiming1001 <512821390@qq.com>
2020-09-21 16:22:54 +0800

    增加整型校验，常量设置

---
+++ src/main/java/com/thoughtworks/basic/ConstantCodeAndValue.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.basic;
+
+public class ConstantCodeAndValue {
+
+	/** flag为l **/
+	public static final String CODE_FLAG_L = "l";
+	/** flag为p **/
+	public static final String CODE_FLAG_P = "p";
+	/** flag为d **/
+	public static final String CODE_FLAG_D = "d";
+	/** value为空 **/
+	public static final String CODE_VALUE_NULL = "";
+	
+}
--- src/test/java/com/thoughtworks/basic/SchemaTest.java
+++ src/test/java/com/thoughtworks/basic/SchemaTest.java
@@ -17,11 +17,24 @@ public class SchemaTest {
 	public void should_return_boolean_when_get_type_given_flag(){
 		//given
 		Set<FlagSchema> flagSchema = new HashSet<>();
-		flagSchema.add(new FlagSchema("l",Boolean.TYPE));
+		flagSchema.add(new FlagSchema(ConstantCodeAndValue.CODE_FLAG_L,Boolean.TYPE));
 		Schema schema = new Schema(flagSchema);
 		//when
-		Object type = schema.getTypeOf("l");
+		Object type = schema.getTypeOf(ConstantCodeAndValue.CODE_FLAG_L);
 		//then
 		assertEquals(Boolean.TYPE,type);
 	    }
+	
+	@Test
+	public void should_return_Integer_when_get_type_given_flag(){
+		//given
+		Set<FlagSchema> flagSchema = new HashSet<>();
+		flagSchema.add(new FlagSchema(ConstantCodeAndValue.CODE_FLAG_P,Integer.TYPE));
+		Schema schema = new Schema(flagSchema);
+		//when
+		Object type = schema.getTypeOf(ConstantCodeAndValue.CODE_FLAG_P);
+		//then
+		assertEquals(Integer.TYPE,type);
+	    }
+	
 }
